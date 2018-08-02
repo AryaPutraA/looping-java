98f4501c69038745c91fef97bd56d0e501bbee29
asus <asus@LAPTOP-1G900J8S>
2018-08-02 07:47:14 +0700

    For While Do-While

---
+++ src/js08/JS08.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package js08;
+
+/**
+ *
+ * @author asus
+ */
+public class JS08 {
+    public static void main(String[] args) {
+         
+        //For
+        int a;
+        for(a = 1;a <=5; a++){
+            System.out.println("SMK Telkom Malang");}
+        
+        //While
+        int b = 1;
+        while(b <= 5){
+            System.out.println("SMK Telkom Malang");
+            b++;}
+        
+        //DO While
+        int c = 1;
+        do{
+            System.out.println("SMK Telkom Malang");
+            c++;}
+        while(c <= 5);
+    
+
+    }
+    
+}
