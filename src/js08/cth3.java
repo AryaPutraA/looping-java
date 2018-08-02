64cdb5aaf94dc5cfbf47eb994c860a2c053c963c
asus <asus@LAPTOP-1G900J8S>
2018-08-02 09:43:34 +0700

    Ganjil Genap

---
+++ src/js08/cth3.java
@@ -0,0 +1,51 @@
+package js08;
+import java.util.*;
+public class cth3 {
+    public static void main(String[] args){
+        
+        //CARA I
+        
+        //MENAMPILKAN 10 BIL GANJIL PERTAMA
+        int x = 1;
+        
+        for(x = 1; x <= 20; x+=2)
+        {
+            System.out.print(x + " ");
+        }
+        
+        System.out.println();
+        
+        //MENAMPILKAN 10 BIL GENAP PERTAMA
+        x = 2;
+        
+        for(x = 2; x <= 20; x+=2)
+        {
+            System.out.print(x + " ");
+        }
+        
+        System.out.println();
+        
+        //CARA II
+        // MENAMPILKAN 10 BIL GANJIL PERTAMA
+          for(x = 1; x <= 20; x++)
+        {
+            if(x%2==1)
+            {
+            System.out.print(x + " ");
+        }}
+            
+            System.out.println();
+        
+        //MENAMPILKAN 10 BIL GENAP PERTAMA    
+          for(x = 1; x <= 20; x++)
+        {
+            if(x%2==0)
+            {
+            System.out.print(x + " ");
+        
+        
+    }
+    
+}
+        }
+    }
